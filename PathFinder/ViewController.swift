35a071ad33fae1d6c7e590988b3b0ce7dadbd258
Artemas Radik <artemasr21@bronxvilleschool.org>
2020-03-23 15:07:41 -0400

    basic button drag detection

--- PathFinder/ViewController.swift
+++ PathFinder/ViewController.swift
@@ -14,7 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        guard let touch = event?.allTouches?.first else {return}
+        let touchLocation = touch.location(in: self.view)
+        buttons.forEach { (button) in
+            if button.frame.contains(touchLocation) {
+                button.backgroundColor = UIColor.systemRed
+            }
+        }
+    }
+    
 }
 
